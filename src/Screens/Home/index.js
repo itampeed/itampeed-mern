1e7e1055885284fd3266ab9eb1096e253b5cc4a5
Muhammad Bilal <itampeed@gmail.com>
2024-03-14 13:46:32 +0500

    3rd changes

--- src/Screens/Home/index.js
+++ src/Screens/Home/index.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import './index.css'
+import ProfilePic from '../../Elements/Profilepicture.webp'
 
 export default function Home() {
-  return (
+  return ( 
     <div className='main-home'>
-      <div className='column'></div>
+      <div className='column'>
+        <div className='intro'>
+          <img src={ProfilePic} alt='profile pic'/>
+          <h3>Muhammad Bilal Safdar</h3>
+          <p>My talent is testament to the beauty that lies within the delicate details of my designs, they are simple, beautiful and adorable. Join me for creativity.</p>
+        </div>
+      </div>
       <div className='column'></div>
       <div className='column'></div>
     </div>
