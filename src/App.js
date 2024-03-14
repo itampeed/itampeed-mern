7c153c3df87816f963e023ca6764d06a86576a30
Muhammad Bilal <itampeed@gmail.com>
2024-03-14 12:36:03 +0500

    Bilals first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './Screens/Home/index.js';
+import Contact from './Screens/Contact/index.js';
+import Youtube from './Screens/Youtube/index.js';
+import Projects from './Screens/Projects/index.js';
+import Navbar from './Components/Navbar/index.js';
+import Login from './Screens/Login/index.js';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <>
+        <Navbar />
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route exact path='/youtube' element={<Youtube />} />
+          <Route exact path='/projects' element={<Projects />} />
+          <Route exact path='/contact' element={<Contact />} />
+          <Route exact path='/login' element={<Login />} />
+        </Routes>
+      </>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/Components/Navbar/index.js
@@ -0,0 +1,62 @@
+import React, { useEffect, useState } from 'react';
+import './index.css';
+import { Link, useLocation } from 'react-router-dom';
+
+export default function Index() {
+  const [menu, setmenu] = useState('menu-items');
+
+  const MenuToggle = () => {
+    setmenu(menu === 'menu-items' ? 'menu-items-mobile' : 'menu-items');
+  };
+
+  let location = useLocation()
+
+  useEffect(()=>{
+    setmenu('menu-items')
+    console.log(location)
+  }, [location])
+
+  return (
+    <div className='menu'>
+      <div className='logo'>
+        <Link to='/'>
+          itampeed
+        </Link>
+      </div>
+      <div className={menu}>
+        <div className='menu-navigation'>
+          <ul>
+            <li>
+              <Link to='/' className={`${location.pathname === '/'? "active": ""}`}>
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link to='/youtube' className={`${location.pathname === '/youtube'? "active": ""}`}>
+                Youtube
+              </Link>
+            </li>
+            <li>
+              <Link to='/portfolio' className={`${location.pathname === '/portfolio'? "active": ""}`}>
+                Portfolio
+              </Link>
+            </li>
+            <li>
+              <Link to='/contact' className={`${location.pathname === '/contact'? "active": ""}`}>
+                Contact
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <div className='menu-account'>
+          <Link to='/login'><button>Login <i class="fa-solid fa-right-to-bracket"></i></button></Link>
+        </div>
+      </div>
+      <div className={`menu-icon ${menu ? 'menu-icon' : ''}`} onClick={MenuToggle}>
+        <div className='line-1'></div>
+        <div className='line-2'></div>
+        <div className='line-3'></div>
+      </div>
+    </div>
+  );
+}
